7580f678dfd4c1f2c36a68df445e5a9b3b46efeb
navv-r <navendra604@gmail.com>
2026-03-15 19:17:01 -0400

    completed the top sellers section along with skeleton loading state and proper routing to authors page

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
-
   const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSellers = async () => {
       try {
-
         const response = await axios.get(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers",
         );
 
         setSellers(response.data);
 
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
       } catch (error) {
         console.error("Error fetching sellers:", error);
       }
@@ -25,13 +28,8 @@ const TopSellers = () => {
     fetchSellers();
   }, []);
 
-  const column1 = sellers.slice(0, 4);
-  const column2 = sellers.slice(4, 8);
-  const column3 = sellers.slice(8, 12);
-
   const renderSeller = (seller, index) => (
     <div className="author_list" key={seller.authorId}>
-
       <div className="author_list_pp">
         <Link to={`/author/${seller.authorId}`}>
           <img src={seller.authorImage} alt={seller.authorName} />
@@ -45,42 +43,62 @@ const TopSellers = () => {
         </Link>
         <span>{seller.price} ETH</span>
       </div>
-
     </div>
+  );
+
+  const renderSkeleton = (index) => (
+    <li key={index}>
+      <div className="author_list_pp">
+        <Skeleton width="50px" height="50px" borderRadius="50%" />
+      </div>
+      <div className="author_list_info">
+        <Skeleton width="120px" height="16px" />
+        <Skeleton width="60px" height="12px" />
+      </div>
+    </li>
   );
 
   return (
     <section id="section-popular" className="no-bottom">
-
       <div className="container">
-
         <div className="row">
-
-          <div className="col-lg-12 text-center">
-            <h2>Top Sellers</h2>
-            <div className="small-border bg-color-2"></div>
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Top Sellers</h2>
+              <div className="small-border bg-color-2"></div>
+            </div>
           </div>
-
         </div>
 
         <div className="row">
+          <div className="col-lg-12">
+            <ol className="author_list">
+              {loading
+                ? new Array(12).fill(0).map((_, index) => renderSkeleton(index))
+                : sellers.map((seller, i) => (
+                    <li key={seller.authorId}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${seller.authorId}`}>
+                          <img
+                            src={seller.authorImage}
+                            alt={seller.authorName}
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
 
-          <div className="col-lg-4">
-            {column1.map((seller, i) => renderSeller(seller, i))}
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId}`}>
+                          {i + 1}. {seller.authorName}
+                        </Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
+            </ol>
           </div>
-
-          <div className="col-lg-4">
-            {column2.map((seller, i) => renderSeller(seller, i + 4))}
-          </div>
-
-          <div className="col-lg-4">
-            {column3.map((seller, i) => renderSeller(seller, i + 8))}
-          </div>
-
         </div>
-
       </div>
-
     </section>
   );
 };
