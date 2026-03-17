4c7a29b2ccad966c262dee52192e93991ef39995
navv-r <navendra604@gmail.com>
2026-03-17 13:14:48 -0400

    removed link from top sellers section

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
